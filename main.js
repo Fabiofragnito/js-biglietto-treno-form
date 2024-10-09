46682cff57dae061e837108986fb690ff357cfb4
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-09 16:43:39 +0200

    recuperto i valori degli input

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('it works');
+function calculateTicketPrice(event) {
+    // Impedisce il comportamento predefinito del form (refresh della pagina)
+    event.preventDefault();
+
+    // Recupera i valori dagli input
+    const km = document.getElementById('km').value;
+    const age = document.getElementById('age').value;
+
+  
+}
+
+
